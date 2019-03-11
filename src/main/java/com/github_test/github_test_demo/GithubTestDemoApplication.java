b3e3eebc6910a91c09a0a6020b0f7a6287cafe5b
runoob <test@runoob.com>
2019-03-11 15:57:10 +0800

    wwdsasddsdss

---
+++ src/main/java/com/github_test/github_test_demo/GithubTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github_test.github_test_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github_test/github_test_demo/GithubTestDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github_test.github_test_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubTestDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
