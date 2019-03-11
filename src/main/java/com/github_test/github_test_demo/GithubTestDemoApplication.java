93500d33370b4646d0083d52145cfbe065a586bd
runoob <test@runoob.com>
2019-03-11 16:04:25 +0800

    ds4444

--- src/main/java/com/github_test/github_test_demo/GithubTestDemoApplication.java
+++ src/main/java/com/github_test/github_test_demo/GithubTestDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubTestDemoApplication {
 
     public static void main(String[] args) {
+        int i = 1;
         SpringApplication.run(GithubTestDemoApplication.class, args);
     }
 
